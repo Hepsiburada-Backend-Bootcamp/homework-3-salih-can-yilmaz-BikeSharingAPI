1ef5980b3f63985175895821fe2399717bf67a8c
Can YILMAZ <canyilmaz999@gmail.com>
2021-10-09 02:05:42 +0300

    HelperResponse UnitTest

---
+++ BikeSharing.Application.Test/UserServiceGetUserTest.cs
@@ -0,0 +1,38 @@
+﻿using BikeSharing.Application.DTOs.Users;
+using BikeSharing.Application.Services.IServices;
+using BikeSharing.Domain.Enums;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeSharingAPI.Test
+{
+    public class UserServiceGetUserTest
+    {
+        public void GetUserTest()
+        {
+            var mock = new Mock<IUserService>();
+
+            mock.Setup(service => service.GetUser(It.IsAny<int>())).Returns(GetMockServiceUser());
+        }
+
+        public UserReadDTO GetMockServiceUser()
+        {
+            return new UserReadDTO
+            {
+                Id = 1,
+                Balance = 50.2,
+                BirthDate = DateTime.Now.AddYears(-25),
+                EMail = "asd@gmail.com",
+                DateJoined = DateTime.Now.AddDays(-15),
+                Gender = EnumGender.M,
+                Name = "Testtt",
+                PhoneNumber = "123456789",
+                Surname = "Testson"
+            };
+        }
+    }
+}
---
+++ BikeSharingAPI.Test/HelperResponseTest.cs
@@ -0,0 +1,41 @@
+﻿using BikeSharingAPI.Enums;
+using BikeSharingAPI.Helpers;
+using BikeSharingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Xunit;
+
+
+namespace BikeSharingAPI.Test
+{
+    public class HelperResponseTest
+    {
+        [Theory]
+        [MemberData(nameof(JsonDataProvider))]
+        public void JsonResultTest(string expected, object data)
+        {
+            var result = HelperResponse.GenerateResponse(
+                    EnumResponseFormat.JSON,
+                    HttpStatusCode.OK,
+                    data
+                );
+
+            var contentResult = result as ContentResult;
+
+            Assert.Equal(expected, contentResult.Content);
+        }
+
+        public static IEnumerable<object[]> JsonDataProvider =>
+           new List<object[]>
+           {
+                new object[] {"{\"Id\":1,\"Name\":\"TestName\"}", new { Id = 1, Name = "TestName" }},
+                new object[] { "[1,2,3,4,5]", new object[]{ 1,2,3,4,5} },
+                new object[] { "[\"elma\",\"armut\",\"mandalina\"]", new List<object> { "elma", "armut", "mandalina"} },
+                new object[] { "{\"ErrorMessage\":\"There is an error\"}", new ErrorModel { ErrorMessage = "There is an error"} },
+                new object[] { "[{\"Id\":1,\"Name\":\"TestName1\"},{\"Id\":2,\"Name\":\"TestName2\"},{\"Id\":3,\"Name\":\"TestName3\"}]",
+                        new List<object> { new { Id = 1, Name = "TestName1" }, new { Id = 2, Name = "TestName2" }, new { Id = 3, Name = "TestName3" } } }
+           };
+    }
+}
